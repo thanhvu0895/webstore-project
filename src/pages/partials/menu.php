e41c8b13c617ffce60489c2d24a3b2bff65ea5fc
Thanh Vu <thanh.vu15@kzoo.edu>
2022-11-08 18:33:36 -0500

    Adding all files to repo

---
+++ src/pages/partials/menu.php
@@ -0,0 +1,49 @@
+<?php 
+$displayNone = (!isset($_SESSION["email"]))  ? "style='display:none'" : '';
+?>
+
+<style>
+    .menu {
+        width: 100%;
+        height: 50px;
+        background-color: #ffd580;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        overflow: hidden;
+    }
+
+    .menu p {
+        margin-left: 5px;
+        margin-right: 5px;
+        margin-bottom: 0px;
+        padding: 3px;
+    }
+
+    .menu :hover {
+        cursor: pointer;
+        border: 2px solid rgb(253, 240, 213);
+        border-radius: 5px;
+    }
+</style>
+
+<div class="menu" <?php 
+    echo $displayNone
+?>>
+    <form id="formFashion" action="catalog.php?category=Fashion" method="post">
+    <p onclick="document.getElementById('formFashion').submit();">Fashion</p>
+    </form>
+
+    <form id="formElectronic" action="catalog.php?category=Electronics" method="post">
+    <p onclick="document.getElementById('formElectronic').submit();">Electronics</p>
+    </form>
+
+    <form id="formTumblers" action="catalog.php?category=Tumblers" method="post">
+    <p onclick="document.getElementById('formTumblers').submit();">Tumblers</p>
+    </form>
+    
+    <form id="formBags" action="catalog.php?category=Bags" method="post">
+    <p onclick="document.getElementById('formBags').submit();">Bags</p>
+    </form>
+
+</div>
