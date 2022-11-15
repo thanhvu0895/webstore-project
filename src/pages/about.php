7fdbb76f0cde19e9bd091a2e0f19ad97952ee1f9
Thanh Vu <thanh.vu15@kzoo.edu>
2022-11-15 18:15:00 -0500

    add changes to aboutus

--- src/pages/about.php
+++ src/pages/about.php
@@ -6,103 +6,71 @@ require_once("connection.php");
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/about-us.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-  <link rel="stylesheet" href="../css/about.css">
+  <title>About us</title>
 </head>
 
 <body>
-  <div>
-    <?php include("partials/header.php") ?>
-    <div style="height: 70vh; display: flex; width: 100vw; justify-content:center; align-content: center;">
-      <div class="column">
-        <div class="card">
-          <div class="container">
-            <div style="height:120px; display:flex; justify-content:center; align-items:center">
-              <h2>Duure</h2>
-            </div>
-            <div class="pfp">
-                <img src="../images/Duure.png" alt="Duure" style="width:210px;height:auto;">
-              </div> <br>
-            <p class="title">Front-End Lead & Back-End Dev</p>
-            <p>Year: Senior</p>
-            <p>Major: CS & Econ</p>
-          </div>
+
+  <?php include("partials/header.php") ?>
+  <div class="cart-container">
+    <div class="product-container">
+      <div class='cart-item'>
+        <img src="../images/Duure.png" alt="dom" height=200 width=200>
+        <div class='item-details'>
+          <h2>Duure</h2>
+          <p class="title">Front-End and UI/UX Lead&nbsp;&nbsp;&nbsp;</p>
+          <p>Year: Senior</p>
+          <p>Major: CS & Econ</p>
         </div>
       </div>
-
-      <div class="column">
-        <div class="card">
-          <div class="container">
-            <div style="height:120px; display:flex; justify-content:center; align-items:center">
-              <h2>Thanh</h2>
-            </div>
-            <div class="pfp">
-                <img src="../images/Thanh.png" alt="Thanh" style="width:210px;height:auto;">
-              </div> <br>
-            <p class="title">Back-End and Database Lead</p>
-            <p>Year: Senior</p>
-            <p>Major: CS</p>
-          </div>
+      <div class='cart-item'>
+        <img src="../images/Thanh.png" alt="dom" height=200 width=200>
+        <div class='item-details'>
+          <h2>Thanh</h2>
+          <p class="title">Back-End and Database Lead&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+          <p>Year: Senior</p>
+          <p>Major: CS</p>
         </div>
       </div>
-
-      <div class="column">
-        <div class="card">
-          <div class="container">
-            <div style="height:120px; display:flex; justify-content:center; align-items:center">
-              <h2>William</h2>
-            </div>
-            <div class="pfp">
-                <img src="../images/William.png" alt="William" style="width:210px;height:auto;">
-              </div> <br>
-            <p class="title">Back-End Dev</p>
-            <p>Year: Senior</p>
-            <p>Major: CS</p>
-          </div>
+      <div class='cart-item'>
+        <img src="../images/William.png" alt="dom" height=200 width=200>
+        <div class='item-details'>
+          <h2>William</h2>
+          <p class="title">Back-End Dev</p>
+          <p>Year: Senior</p>
+          <p>Major: CS</p>
         </div>
       </div>
-
-      <div class="column">
-        <div class="card">
-          <div class="container">
-            <div style="height:120px; display:flex; justify-content:center; align-items:center">
-              <h2>Caroline</h2>
-            </div>
-            <div class="pfp">
-                <img src="../images/Caroline.jpg" alt="Caroline" style="width:210px;height:auto;">
-              </div> <br>
-            <p class="title">Front-End Dev</p>
-            <p>Year: Senior</p>
-            <p>Major: CS</p>
-          </div>
+      <div class='cart-item'>
+        <img src="../images/Caroline.jpg" alt="dom" height=200 width=200>
+        <div class='item-details'>
+          <h2>Caroline</h2>
+          <p class="title">Front-End Dev</p>
+          <p>Year: Senior</p>
+          <p>Major: CS</p>
         </div>
       </div>
-
-      <div class="column">
-        <div class="card">
-          <div class="container">
-            <div style="height:120px; display:flex; justify-content:center; align-items:center">
-              <h2>Sophie</h2>
-            </div>
-            <div class="pfp">
-                <img src="../images/Sophie.png" alt="Sophie" style="width:210px;height:auto;">
-              </div> <br>
-            <p class="title">UI/UX Lead</p>
-            <p>Year: Sophomore</p>
-            <p>Major: CS</p>
-          </div>
+      <div class='cart-item'>
+        <img src="../images/Sophie.png" alt="dom" height=200 width=200>
+        <div class='item-details'>
+          <h2>Sophie</h2>
+          <p class="title">UI/UX Lead</p>
+          <p>Year: Sophomore</p>
+          <p>Major: CS</p>
         </div>
       </div>
     </div>
-    <?php include("partials/footer.php") ?>
+  </div>
+  <?php include("partials/footer.php") ?>
+
 </body>
 
 </html>
