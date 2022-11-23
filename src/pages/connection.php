7974c279480785aa4bea2bc25baa310f759ba119
Thanh Vu <thanh.vu15@kzoo.edu>
2022-11-23 08:25:17 -0500

    connection

--- src/pages/connection.php
+++ src/pages/connection.php
@@ -1,10 +1,10 @@
 <?php
 
-$dbserver = "24.199.95.130";
+$dbserver = "178.128.154.4";
 $dbuser = "root";
 $dbpass = "1234";
 $dbname = "db_webstore";
-$port = "3306";
+$port = "3305";
 
 try {
   $conn = new PDO("mysql:host=$dbserver;port=$port;dbname=$dbname", $dbuser, $dbpass);
